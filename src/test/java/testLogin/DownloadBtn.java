7e7d43a8cee41ae1c57abaa31c40b94efe8a1b38
lavishkhetan <lkhetan@ishir.com>
2016-01-04 17:31:23 +0530

    Code modified for Download functionality.

--- src/test/java/testLogin/DownloadBtn.java
+++ src/test/java/testLogin/DownloadBtn.java
@@ -5,23 +5,28 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class DownloadBtn {
-	
+	String Actualtext;
 	public WebDriverWait wait;
 	public WebDriver driver=null;
 	
-	@Test(priority = 1)
-	public void testSetup() throws Exception {
+	@BeforeTest
+	public void testsetup() {
 		WebDriver driver = new FirefoxDriver();	
 		driver.manage().window().maximize();
 		wait = new WebDriverWait(driver, 5);
 		driver.get("http://www.ishirqa.com");
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 	    System.out.println("Login into the account");
-
-		System.out.println("Entering into DownloadFunctionality method");		
+	}
+	@Test(priority = 1)
+	    public void Downloadbtn_Left() throws Exception {
+		System.out.println("Entering into DownloadFunctionality 1 method");		
 		driver.findElement(By.xpath("//ul[@class='nav navbar-nav']/li[5]")).click();
 	    try {
 			Thread.sleep(1000);
@@ -30,7 +35,7 @@ public class DownloadBtn {
 			e1.printStackTrace();
 		}  
 	    driver.findElement(By.xpath("//div[@class='row']//div[contains(@class,'col-lg-8')]//a")).click();
-	    System.out.println("Download button working");
+	    System.out.println("Download_Left button working");
 	    
 	    String parentHandle = driver.getWindowHandle(); // get the current window handle
 	    for (String winHandle : driver.getWindowHandles())
@@ -45,6 +50,43 @@ public class DownloadBtn {
 	    }
 	    driver.close(); // close newly opened window when done with it
 	    driver.switchTo().window(parentHandle);
-	 }
-
+	    Actualtext = driver.findElement(By.xpath("//div[@class='text-left yello-bg']/h3")).getText();
+     	Assert.assertEquals(Actualtext,"Software Testing for the leading Insurance Company");
+	    
+//	 }
+//	@Test(priority =2)
+//	public void Downloandbtn_Right(){
+//	System.out.println("Entering into DowloandFunctionality 2 Mehtod");
+//	driver.findElement(By.xpath("//ul[@class='nav navbar-nav']/li[5]")).click();
+//    try {
+//		Thread.sleep(1000);
+//	} catch (InterruptedException e1) {
+//		// TODO Auto-generated catch block
+//		e1.printStackTrace();
+//	} 
+     	
+    driver.findElement(By.xpath("//div[@class='row']//div[contains(@class,'col-lg-7')]//a")).click();
+    System.out.println("Download_Right button working");
+    
+    String parentHandle1 = driver.getWindowHandle(); // get the current window handle
+    for (String winHandle : driver.getWindowHandles())
+    {
+    driver.switchTo().window(winHandle); // switch focus of WebDriver to the next found window handle (that's your newly opened window)
+    try {
+		Thread.sleep(1000);
+	} catch (InterruptedException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
 	}
+    }
+    driver.close(); // close newly opened window when done with it
+    driver.switchTo().window(parentHandle1);
+    Actualtext = driver.findElement(By.xpath("//div[@class='text-left green-bg']/h3")).getText();
+ 	Assert.assertEquals(Actualtext,"On Demand Software Testing");
+	}
+	@AfterClass
+	public void tearDown() {
+		driver.quit();
+	}
+	}
+	
--- src/test/java/testLogin/LetsTalk.java
+++ src/test/java/testLogin/LetsTalk.java
@@ -12,50 +12,39 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 
+public class LetsTalk {	
+		public WebDriver driver; 
+		public String expected = null;
+		public String Actualtext;	
+		public WebDriverWait wait;		
 
-public class LetsTalk {
-	public WebDriver driver; 
-	public String expected = null;
-	public String Actualtext;
-	public WebDriverWait wait;
-	
-	@BeforeTest(alwaysRun = true)
-		public void Browser() {
-		driver =  new FirefoxDriver();
-  		driver.manage().window().maximize();
-  		wait = new WebDriverWait(driver, 5);
-		driver.get("http://www.ishirqa.com/");
-		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-		System.out.println("Login into the account");
-	}
-	@Test
-	public void Dashboard(){
-		Actualtext=driver.findElement(By.xpath("//div[@class='logo']/p")).getText();
-	    Assert.assertEquals(Actualtext, "Software Quality Assurance Services");
-	    System.out.println("Verified the Dashboard");
-	}
-	
-	@Test(priority = 1)
- 		public void ScrollPageDown(){
-		JavascriptExecutor jse = (JavascriptExecutor)driver;
-		jse.executeScript("window.scrollBy(0,4000)", "");
-		}	
+		@BeforeTest(alwaysRun = true)
+			public void Browser() {
+				driver =  new FirefoxDriver();
+				driver.manage().window().maximize();
+				wait = new WebDriverWait(driver, 5);
+				driver.get("http://www.ishirqa.com/");
+				driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+				System.out.println("Login into the account");}	
+		@Test
+				public void Dashboard(){
+				Actualtext=driver.findElement(By.xpath("//div[@class='logo']/p")).getText();
+				Assert.assertEquals(Actualtext, "Software Quality Assurance Services");
+				System.out.println("Verified the Dashboard");}
+		@Test(priority = 1) 	
+			public void ScrollPageDown(){		
+				JavascriptExecutor jse = (JavascriptExecutor)driver;	
+				jse.executeScript("window.scrollBy(0,4000)", "");}
 //		This will launch the browser, Verify the Dashboard and scroll down the page 
-		
-	 @Test(priority = 2)
-		public void FormField() throws InterruptedException{
-			Actions.Execute(driver);
-			Thread.sleep(1000);
-			Actualtext = driver.findElement(By.xpath("//section[@id='about']/div/div/div[1]/p")).getText();
-	     	Assert.assertEquals(Actualtext,"Our representative will contact you soon.");
-	     	System.out.println("Assertion Working for Submit page");
-		  }
+		@Test(priority = 2)
+			public void FormField() throws InterruptedException{	
+				Actions.Execute(driver);	
+				Thread.sleep(1000);	
+				Actualtext = driver.findElement(By.xpath("//section[@id='about']/div/div/div[1]/p")).getText();
+				Assert.assertEquals(Actualtext,"Our representative will contact you soon.");
+				System.out.println("Assertion Working for Submit page");}
 //		Once scrolled down, This will fetch all the values of home page FormField from Actions class
-		 		  
-		  @AfterTest
-		  public void afterTest() {		  
-		  driver.quit();
-		  }
-
-		}
-
+		@AfterTest
+		  		public void afterTest() {	
+				driver.quit();}
+	}
